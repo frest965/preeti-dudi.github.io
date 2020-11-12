3d500fcf80082b0be655fb3aa77bf100125c3559
preeti-dudi <64349037+preeti-dudi@users.noreply.github.com>
2020-11-12 13:42:00 +0530

    major change

--- My Organisation/javascript.js
+++ My Organisation/javascript.js
@@ -1,17 +1,12 @@
 window.onload=function(){
     var el=document.getElementById("intro");
-    var str ="Helping each other.......";
+    var str ='helpincovid   We are here....... ';
     var i=0;
     el.innerHTML="";
-    var text=setInterval(addtext,500);
-    function addtext() {
-            if(i==(str.length-1)){
-                clearInterval(text);
-            }
-        else{
-            el.innerHTML=el.innerHTML+str[i];
-            i++;
-        }
-    }
-    
+    var text=setInterval(function(){if(i==(str.length)){clearInterval(text);}else{el.innerHTML=el.innerHTML+str[i];i++;}},500);
+    var ele=document.getElementById("intro1");
+    var stri='How can We help you? ';
+    var j=0;
+    el.innerHTML="";
+    var test=setInterval(function(){if(j==(stri.length)){clearInterval(test);}else{ele.innerHTML=ele.innerHTML+stri[j];j++;}},500);
 }
